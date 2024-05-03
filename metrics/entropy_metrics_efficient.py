8f8c0d7d59bdd4866afc83fc60c80c3e9fbc3dfb
Marta Nahorniuk <76553231+martazavro@users.noreply.github.com>
2024-05-03 17:12:30 +0300

    Update entropy_metrics_efficient.py

--- metrics/entropy_metrics_efficient.py
+++ metrics/entropy_metrics_efficient.py
@@ -1,18 +1,3 @@
-import numpy as np
-
-def nansum_zero_helper(myArr, axis=None):
-    """Handle the summation while ignoring NaNs and returning NaN where all elements are NaN."""
-    myArr = np.asarray(myArr)
-    mySum = np.nansum(myArr, axis=axis)
-    if isinstance(mySum, np.ndarray) and mySum.size == 0:
-        return np.nan
-    if isinstance(mySum, np.ndarray):
-        all_nan = np.all(np.isnan(myArr), axis=axis)
-        if np.any(all_nan):
-            mySum[all_nan] = np.nan
-    return mySum
-
-
 def entropy_metrics_efficient(choice, reward, hr_side, stay=None):
     choice = np.array(choice)
     reward = np.array(reward)
